c541869aee439eed4ecf3f3d9641924ac0d2dea2
liushuai <770722922@qq.com>
2021-07-19 23:33:43 +0800

    dht11

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,83 @@
 #include "game/game_engine_port_arduino.h"
 #include "game/chrome_game.h"
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include "OLED.h"
+
+static DHT_Unified dht(0, DHT11);
+static uint32_t delayMS;
+static uint32_t count = 0;
 
 void setup() {
-    ScreenConfig c{};
-    c.SCREEN_WIDTH = OLED_WIDTH;
-    c.SCREEN_HEIGHT = OLED_HEIGHT;
-    c.PER_CHAR_WIDTH = 6;
-    c.fps = 120;
-    c.canvas = new Screen;
-    start_game(&c);
+    Wire.begin(OLED_SDA, OLED_SCL);
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3c, true, false);
+    display.setTextColor(WHITE);
+
+    Serial.begin(115200);
+    // Initialize device.
+    dht.begin();
+    Serial.println(F("DHTxx Unified Sensor Example"));
+    // Print temperature sensor details.
+    sensor_t sensor;
+    dht.temperature().getSensor(&sensor);
+    Serial.println(F("------------------------------------"));
+    Serial.println(F("Temperature Sensor"));
+    Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+    Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+    Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+    Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+    Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+    Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+    Serial.println(F("------------------------------------"));
+    // Print humidity sensor details.
+    dht.humidity().getSensor(&sensor);
+    Serial.println(F("Humidity Sensor"));
+    Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+    Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+    Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+    Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+    Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+    Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+    Serial.println(F("------------------------------------"));
+    // Set delay between sensor readings based on sensor details.
+    delayMS = sensor.min_delay / 1000;
 }
 
 void loop() {
+    // Delay between measurements.
+    delay(delayMS);
+
+    display.clearDisplay();
+    display.setCursor(0, 20);
+    display.printf("count:%d", count++);
+
+    // Get temperature event and print its value.
+    sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+        Serial.println(F("Error reading temperature!"));
+    }
+    else {
+        Serial.print(F("Temperature: "));
+        Serial.print(event.temperature);
+        Serial.println(F("°C"));
+
+        display.setCursor(0, 0);
+        display.printf("%.2f°C", event.temperature);
+    }
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+        Serial.println(F("Error reading humidity!"));
+    }
+    else {
+        Serial.print(F("Humidity: "));
+        Serial.print(event.relative_humidity);
+        Serial.println(F("%"));
+
+        display.setCursor(0, 10);
+        display.printf("%.2f%%", event.relative_humidity);
+    }
+    display.display();
 }
